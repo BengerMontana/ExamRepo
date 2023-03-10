ce41eeedd5d0c1df79f7cb60f0c0776071b8897b
BengerMontana <108237275+BengerMontana@users.noreply.github.com>
2023-03-11 01:19:40 +0200

    add basic structure

---
+++ src/container/App.tsx
@@ -0,0 +1,9 @@
+import Header from "./Header/Header"
+
+export default function App() {
+    return (
+        <div className="flex bg-gray-400 rounded py-5 px-10">
+            <Header/>
+        </div>
+    )
+}
